906c49dd3017569fbb43cb2997d5e38bc3c3a152
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-12-28 22:18:28 -0300

    correcting exercise nine

--- exercises/warm_up/nine.asm
+++ exercises/warm_up/nine.asm
@@ -1,14 +1,18 @@
     processor 6502
     seg Code
     org $F000
+    
 Start:
     lda #1
-Loop:
-    inc a
-    cmp a, #10
-    jnz Loop
     
-    bne Loop
+Loop:
+    clc
+    adc #1
+    
+    cmp #10 ; compares directly to the accumulator
+    bne Loop ; loops back until A is not equals to 10
+    
+    jmp Start
 
     org $FFFC
     .word Start
