8004aded4c5043065706cbab43afd12e83ac7f8f
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-12-28 21:26:21 -0300

    correcting exercise six

--- exercises/warm_up/six.asm
+++ exercises/warm_up/six.asm
@@ -1,18 +1,29 @@
     processor 6502
     seg Code
     org $F000
+    
 Start:
     lda #1
     ldx #2
     ldy #3
 
-    inc x
-    inc y
-    inc a
+    inx
+    iny
+    
+    clc
+    adc #1
+    
+    ; there's no increment A in 6502
 
-    dec x
-    dec y
-    dec a
+    dex
+    dey
+    
+    sec
+    sbc #1
+    
+    ; there's no decrement A in 6502
+    
+    jmp Start
 
     org $FFFC
     .word Start
