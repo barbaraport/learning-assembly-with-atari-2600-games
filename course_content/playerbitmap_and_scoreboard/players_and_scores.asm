c64c45cd8f9a9b9bf8701f0886807b5a891947a3
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-01-06 21:58:06 -0300

    players 0 and 1 with scoreboards implemented

---
+++ course_content/playerbitmap_and_scoreboard/players_and_scores.asm
@@ -0,0 +1,133 @@
+    processor 6502
+    
+    include "vcs.h"
+    include "macro.h"
+    
+    seg Code
+    
+    org $F000
+    
+Start:
+    CLEAN_START
+    
+    ldx #$80
+    stx COLUBK
+    
+    lda #%1111
+    sta COLUPF
+    
+    lda #$48
+    sta COLUP0
+    
+    lda #$C6
+    sta COLUP1
+
+Frame:
+
+    lda #2 ; turning on v-sync and v-blank
+    sta VBLANK
+    sta VSYNC
+    
+    repeat 3
+    	sta WSYNC ; v-sync scanlines
+    repend
+    
+    lda #0
+    sta VSYNC ; turning off v-sync
+    
+    repeat 37
+        sta WSYNC
+    repend
+    
+    lda #0
+    sta VBLANK ; turning off v-blank
+    
+    ; --------------------------------------------
+    
+VisibleScanlines:
+    repeat 10
+        sta WSYNC
+    repend
+    
+    ldy #0
+ScoreboardLoop:
+    lda NumberBitmap,y
+    sta PF1
+    sta WSYNC
+    iny ; increment y register
+    cpy #10 ; compare y register
+    bne ScoreboardLoop
+    
+    lda #0
+    sta PF1 ; disable playfield
+    
+    repeat 50
+        sta WSYNC
+    repend
+    
+    ldy #0
+Player0Loop:
+    lda PlayerBitmap,y
+    sta GRP0
+    sta WSYNC
+    iny
+    cpy #10
+    bne Player0Loop
+    
+    lda #0
+    sta GRP0 ; disable player 0 graphics
+    
+    ldy #0
+Player1Loop:
+    lda PlayerBitmap,y
+    sta GRP1
+    sta WSYNC
+    iny
+    cpy #10
+    bne Player1Loop
+    
+    lda #0
+    sta GRP1 ; disable player 1 graphics
+    
+    repeat 102
+        sta WSYNC
+    repend
+    
+    ; --------------------------------------------
+    sta VBLANK
+    repeat 30
+        sta WSYNC
+    repend
+    
+    
+    jmp Frame
+    
+    org $FFE8
+PlayerBitmap:
+    .byte #%01111110
+    .byte #%11111111
+    .byte #%10011001
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%10111101
+    .byte #%11000011
+    .byte #%11111111
+    .byte #%01111110
+    
+    org $FFF2
+NumberBitmap:
+    .byte #%00001110
+    .byte #%00001110
+    .byte #%00000010
+    .byte #%00000010
+    .byte #%00001110
+    .byte #%00001110
+    .byte #%00001000
+    .byte #%00001000
+    .byte #%00001110
+    .byte #%00001110
+    
+    org $FFFC
+    .word Start
+    .word Start
