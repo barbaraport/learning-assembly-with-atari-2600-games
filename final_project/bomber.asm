2b0e2ed90cceb19920254a0cd4b05edd35f9bb0d
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2023-01-29 07:55:56 -0300

    fixing identation

--- final_project/bomber.asm
+++ final_project/bomber.asm
@@ -134,73 +134,70 @@ GameVisibleLines:
         jmp StartFrame
         
 JetSprite:
-    .byte #%00000000         ;
-    .byte #%00010100         ;   # #
-    .byte #%01111111         ; #######
-    .byte #%00111110         ;  #####
-    .byte #%00011100         ;   ###
-    .byte #%00011100         ;   ###
-    .byte #%00001000         ;    #
-    .byte #%00001000         ;    #
-    .byte #%00001000         ;    #
+        .byte #%00000000         ;
+        .byte #%00010100         ;   # #
+        .byte #%01111111         ; #######
+        .byte #%00111110         ;  #####
+        .byte #%00011100         ;   ###
+        .byte #%00011100         ;   ###
+        .byte #%00001000         ;    #
+        .byte #%00001000         ;    #
+        .byte #%00001000         ;    #
 
 JetSpriteTurn:
-    .byte #%00000000         ;
-    .byte #%00001000         ;    #
-    .byte #%00111110         ;  #####
-    .byte #%00011100         ;   ###
-    .byte #%00011100         ;   ###
-    .byte #%00011100         ;   ###
-    .byte #%00001000         ;    #
-    .byte #%00001000         ;    #
-    .byte #%00001000         ;    #
+        .byte #%00000000         ;
+        .byte #%00001000         ;    #
+        .byte #%00111110         ;  #####
+        .byte #%00011100         ;   ###
+        .byte #%00011100         ;   ###
+        .byte #%00011100         ;   ###
+        .byte #%00001000         ;    #
+        .byte #%00001000         ;    #
+        .byte #%00001000         ;    #
 
 BomberSprite:
-    .byte #%00000000         ;
-    .byte #%00001000         ;    #
-    .byte #%00001000         ;    #
-    .byte #%00101010         ;  # # #
-    .byte #%00111110         ;  #####
-    .byte #%01111111         ; #######
-    .byte #%00101010         ;  # # #
-    .byte #%00001000         ;    #
-    .byte #%00011100         ;   ###
+        .byte #%00000000         ;
+        .byte #%00001000         ;    #
+        .byte #%00001000         ;    #
+        .byte #%00101010         ;  # # #
+        .byte #%00111110         ;  #####
+        .byte #%01111111         ; #######
+        .byte #%00101010         ;  # # #
+        .byte #%00001000         ;    #
+        .byte #%00011100         ;   ###
 
 JetColor:
-    .byte #$00
-    .byte #$FE
-    .byte #$0C
-    .byte #$0E
-    .byte #$0E
-    .byte #$04
-    .byte #$BA
-    .byte #$0E
-    .byte #$08
+        .byte #$00
+        .byte #$FE
+        .byte #$0C
+        .byte #$0E
+        .byte #$0E
+        .byte #$04
+        .byte #$BA
+        .byte #$0E
+        .byte #$08
 
 JetColorTurn:
-    .byte #$00
-    .byte #$FE
-    .byte #$0C
-    .byte #$0E
-    .byte #$0E
-    .byte #$04
-    .byte #$0E
-    .byte #$0E
-    .byte #$08
+        .byte #$00
+        .byte #$FE
+        .byte #$0C
+        .byte #$0E
+        .byte #$0E
+        .byte #$04
+        .byte #$0E
+        .byte #$0E
+        .byte #$08
 
 BomberColor:
-    .byte #$00
-    .byte #$32
-    .byte #$32
-    .byte #$0E
-    .byte #$40
-    .byte #$40
-    .byte #$40
-    .byte #$40
-    .byte #$40
-        
-        
-        
+        .byte #$00
+        .byte #$32
+        .byte #$32
+        .byte #$0E
+        .byte #$40
+        .byte #$40
+        .byte #$40
+        .byte #$40
+        .byte #$40
         
         org $FFFC
         .word Reset
